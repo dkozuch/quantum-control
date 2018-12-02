4e78668be2c7540d3021318ba9b3ad4fe31cd02e
Yi-Che Chang <y.chang@princeton.edu>
2018-12-02 02:36:22 -0500

    Added fieldSolver.py and functions.py

---
+++ functions.py
@@ -0,0 +1,23 @@
+'''functions.py
+'''
+
+import numpy as np
+from dataStore import DataStore
+
+def integrate(self):
+	"""Integrate Schroedinger equation one time step forward.
+
+	"""
+	pass
+
+def pathFromField(self):
+	"""Calculate predicted path from a given field.
+
+	Note: state can be provided optionally to skip integration.
+
+	"""
+	pass
+
+def d2dt2(self):
+    """Calculate second derivative using centered finite differences."""
+	pass
